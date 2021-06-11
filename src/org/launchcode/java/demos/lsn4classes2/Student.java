a56e3391c69bea8b44f164a12940b990d8b71fca
Andrew <narflz@gmail.com>
2021-06-11 16:18:45 -0500

    2B exercises done

--- src/org/launchcode/java/demos/lsn4classes2/Student.java
+++ src/org/launchcode/java/demos/lsn4classes2/Student.java
@@ -1,5 +1,7 @@
 package org.launchcode.java.demos.lsn4classes2;
 
+import java.util.Objects;
+
 public class Student {
 
     private static int nextStudentId = 1;
@@ -30,20 +32,47 @@ public class Student {
 
 
      //TODO: Uncomment and complete the getGradeLevel method here:
-//    public String getGradeLevel() {
-//        // Determine the grade level of the student based on numberOfCredits
-//    }
+    public String getGradeLevel(int currentCredits) {
+        // Determine the grade level of the student based on numberOfCredits
+        if (currentCredits <= 29) {
+            return "Freshman";
+        } else if (currentCredits <= 59) {
+            return "Sophomore";
+        } else if (currentCredits <= 89) {
+            return "Junior";
+        } else {
+            return "Senior";
+        }
+    }
 
     // TODO: Complete the addGrade method.
     public void addGrade(int courseCredits, double grade) {
         // Update the appropriate fields: numberOfCredits, gpa
+        double qualityScore = this.gpa * this.numberOfCredits;
+        qualityScore += courseCredits * grade;
+        this.numberOfCredits += courseCredits;
+        this.gpa = qualityScore / this.numberOfCredits;
     }
 
     // TODO: Add your custom 'toString' method here. Make sure it returns a well-formatted String rather
     //  than just the class fields.
 
+    public String toString() {
+        String studentInfo = this.name + " is a " + this.getGradeLevel(this.numberOfCredits) + " with " + this.numberOfCredits + " credits and a " + this.gpa + " GPA.";
+        return studentInfo;
+    }
+
     // TODO: Add your custom 'equals' method here. Consider which fields should match in order to call two
     //  Student objects equal.
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Student)) return false;
+        Student student = (Student) o;
+        return getStudentId() == student.getStudentId();
+    }
 
     public String getName() {
         return name;
